b431ae518fca852a5ba073911c5a532b50699973
Thoufiq Yahya <thawfeek_yahya@yahoo.co.in>
2025-02-15 12:21:43 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,28 @@
-  -- Key mappings                                                                                                                                                                                           
+-- Telescope Key mappings
 vim.keymap.set('n', '<Leader>ff', '<cmd>Telescope find_files<CR>', { desc = 'Find files' })
 vim.keymap.set('n', '<Leader>fg', '<cmd>Telescope live_grep<CR>', { desc = 'Live grep' })
 vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { desc = 'Find Buffers' })
 vim.keymap.set('n', '<leader>fr', '<cmd>Telescope oldfiles<CR>', { desc = 'Find Recent Files' })
+vim.keymap.set("n", "<leader>gL", ":Telescope git_bcommits<CR>", { desc = "View Git Log for Current File" })
+
+
+
+-- ey mappings
 vim.keymap.set('n', '<Leader>e', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' })
 vim.keymap.set('n', '<Leader>t', '<cmd>TagbarToggle<CR>', { desc = 'Toggle Tagbar' })
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = "Show diagnostics" })
 vim.keymap.set("n", "<leader>nh", ":nohlsearch<CR>", { noremap = true, silent = true })
 
- 
 
+-- Git shortcuts
+vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview Git Hunk" })
+vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { desc = "Reset Git Hunk" })
+vim.keymap.set("n", "<leader>gd", ":Gitsigns diffthis<CR>", { desc = "Git Diff (Current vs HEAD)" })
+vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", { desc = "Git Blame" })
+
+
+-- Git (LazyGit)
+vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { desc = "Open LazyGit" })
 
 -- Settings
 vim.opt.cursorline = true
@@ -27,11 +41,28 @@ require('packer').startup(function(use)
   use 'junegunn/fzf.vim'
   use 'preservim/tagbar'
 
+  use { 
+     "lewis6991/gitsigns.nvim",
+      requires = "nvim-lua/plenary.nvim" 
+  }
+
+  -- Lazy Git
+  use {
+    "kdheepak/lazygit.nvim",
+     requires = { "nvim-lua/plenary.nvim" }
+  }
+
+  -- Show marks
+  use "kshenoy/vim-signature"
+
+
   -- Language Server Protocol (LSP)
   use "neovim/nvim-lspconfig"   -- Base LSP support
   use "hrsh7th/nvim-cmp"        -- Autocompletion
   use "hrsh7th/cmp-nvim-lsp"    -- LSP completion source
 
+  use "mfussenegger/nvim-lint" 
+  use "mfussenegger/nvim-dap"  -- Debugging support
 
   use {
   'nvim-tree/nvim-tree.lua',
@@ -62,3 +93,46 @@ require('packer').startup(function(use)
   use "MaxMEllon/vim-jsx-pretty"  -- JSX/TSX support
 
 end)
+
+
+-- Enable Treesitter for better syntax highlighting
+require("nvim-treesitter.configs").setup {
+  ensure_installed = { "cpp", "qmljs", "javascript", "typescript", "tsx", "json", "html", "css" },
+  highlight = { enable = true },
+}
+
+-- Setup LSP for C++ (Qt), QML, JavaScript
+local lspconfig = require("lspconfig")
+
+-- Enable Clangd for C++ (Qt)
+lspconfig.clangd.setup({
+  cmd = { "clangd", "--background-index" },
+  filetypes = { "cpp", "c", "objc", "objcpp", "h", "hpp" },
+})
+
+-- Enable QML LSP (qmlls)
+lspconfig.qmlls.setup({
+  cmd = { "qmlls" },  -- Make sure you have qmlls6 installed
+  filetypes = { "qml" },
+})
+
+-- Enable LSP diagnostics
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+})
+
+
+require("gitsigns").setup {
+  signs = {
+    add          = { text = "+" },
+    change       = { text = "~" },
+    delete       = { text = "-" },
+    topdelete    = { text = "‾" },
+    changedelete = { text = "~" },
+  },
+  numhl = false,
+  linehl = false,
+  word_diff = false,
+}
