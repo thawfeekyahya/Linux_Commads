283d0b0a050f8103c588af42774fb543960b59a5
Thawfeek Yahya <thawfeek_yahya@yahoo.co.in>
2025-03-16 21:49:35 +0530

    Update status line

--- nvim_config/lua/plugins/lualine.lua
+++ nvim_config/lua/plugins/lualine.lua
@@ -1,7 +1,8 @@
 require('lualine').setup({
   options = {
     theme = 'auto', -- Change theme
-    section_separators = '', component_separators = ''
+    section_separators = { left = '', right = '' },  -- Arrow effect
+    component_separators = { left = '', right = '' }, -- Small arrows
   },
   sections = {
     lualine_c = { 'filename' },
