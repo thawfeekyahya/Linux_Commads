369c2fff03e949fd7da972a8e4fb2430f56e21de
Thawfeek Yahya <thawfeek_yahya@yahoo.co.in>
2025-03-22 06:53:01 +0530

    removing duplicate keybinding

--- nvim_lazy/lua/plugins/nvimtree.lua
+++ nvim_lazy/lua/plugins/nvimtree.lua
@@ -14,14 +14,6 @@ return {
         -- Default mappings
         api.config.mappings.default_on_attach(bufnr)
 
-        -- Custom keybinding for opening a file in system explorer
-        vim.keymap.set("n", "<C-e>", function()
-          local node = api.tree.get_node_under_cursor()
-          if node and node.absolute_path then
-            local open_cmd = vim.fn.has("mac") == 1 and "open" or "xdg-open"
-            vim.fn.system(open_cmd .. " '" .. node.absolute_path .. "' &")
-          end
-        end, opts("Open in system explorer"))
       end
     }
   end
