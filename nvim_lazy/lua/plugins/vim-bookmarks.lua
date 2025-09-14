e85f2085c48511886390c6cf8d0c0aa8bed72e31
Thoufiq Yahya <xpheno.yahya@mercedes-benz.com>
2025-09-14 13:20:31 +0530

    Fix default prev and next jump bindings

--- nvim_lazy/lua/plugins/vim-bookmarks.lua
+++ nvim_lazy/lua/plugins/vim-bookmarks.lua
@@ -1,5 +1,8 @@
 return {
   'MattesGroeger/vim-bookmarks',
+  init =  {
+    vim.g.bookmark_no_default_key_mappings = 1
+  },
   dependencies = {
     'nvim-telescope/telescope.nvim',
     {
@@ -13,5 +16,9 @@ return {
     vim.g.bookmark_save_per_working_dir = 1
     vim.g.bookmark_auto_save = 1
     vim.g.bookmark_auto_close = 1
+    vim.api.nvim_set_keymap("n", "mm", "<Plug>BookmarkToggle", {})
+    vim.api.nvim_set_keymap("n", "mi", "<Plug>BookmarkAnnotate", {})
+    vim.api.nvim_set_keymap("n", "mn", "<Plug>BookmarkNext", {})
+    vim.api.nvim_set_keymap("n", "mp", "<Plug>BookmarkPrev", {})
   end
 }
