cef9fbbde2a3f4e414df854e3cc83bb0a45960af
Thawfeek Yahya <thawfeek_yahya@yahoo.co.in>
2026-03-26 11:31:17 +0530

    Add Copilotchat module

--- nvim_lazy/lua/plugins/ai.lua
+++ nvim_lazy/lua/plugins/ai.lua
@@ -27,5 +27,16 @@ return {
     },
     {
         "github/copilot.vim"
-    }
+    },
+        {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            { "nvim-lua/plenary.nvim", branch = "master" },
+        },
+        build = "make tiktoken",
+        opts = {
+          -- See Configuration section for options
+        },
+    },
+
 }
