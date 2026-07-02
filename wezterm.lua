5e0ef8dc8c76650964ca660c518d174b9232f51c
Thawfeek Yahya <thawfeek_yahya@yahoo.co.in>
2026-07-02 13:32:51 +0530

    update for crossplatform

--- wezterm.lua
+++ wezterm.lua
@@ -2,86 +2,85 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+-- Detect OS
+local is_macos = wezterm.target_triple:find("darwin") ~= nil
+
+-- Platform-specific modifiers
+local CMD = is_macos and "CMD" or "CTRL|SHIFT"
+local CMD_SHIFT = is_macos and "CMD|SHIFT" or "CTRL|ALT"
+
 config.initial_cols = 120
 config.initial_rows = 28
 
 config.font = wezterm.font("FiraCode Nerd Font Mono")
-config.font_size = 20 
+config.font_size = 20
 
---- Available color schemes: https://wezfurlong.org/wezterm/colorschemes/index.html
---- Catppuccin Mocha
----Catppuccin Macchiato
----Tokyo Night
----Gruvbox Dark
----Dracula
----OneDark (base16)
----Nord
----Solarized Dark
----Builtin Solarized Dark
----GitHub Dark
----Kanagawa (Dragon)
----Kanagawa (Wave)
----Night Owl
----Everforest Dark (Hard)
----Monokai Pro
----AdventureTime
-config.color_scheme = 'Catppuccin Macchiato'
+-- Available color schemes:
+-- https://wezfurlong.org/wezterm/colorschemes/index.html
+config.color_scheme = "Catppuccin Macchiato"
 
 config.keys = {
-  -- New tab
+  ----------------------------------------------------------------------
+  -- Tabs
+  ----------------------------------------------------------------------
   {
     key = "t",
-    mods = "CMD",
+    mods = CMD,
     action = wezterm.action.SpawnTab("CurrentPaneDomain"),
   },
 
-  -- Switch tabs
   {
     key = "[",
-    mods = "CMD",
+    mods = CMD,
     action = wezterm.action.ActivateTabRelative(-1),
   },
 
   {
     key = "]",
-    mods = "CMD",
+    mods = CMD,
     action = wezterm.action.ActivateTabRelative(1),
   },
 
-  -- Split into top and bottom
+  ----------------------------------------------------------------------
+  -- Split panes
+  ----------------------------------------------------------------------
   {
     key = "d",
-    mods = "CMD",
+    mods = CMD,
     action = wezterm.action.SplitHorizontal {
       domain = "CurrentPaneDomain",
     },
   },
 
-  -- Split into left and right
   {
     key = "d",
-    mods = "CMD|SHIFT",
+    mods = CMD_SHIFT,
     action = wezterm.action.SplitVertical {
       domain = "CurrentPaneDomain",
     },
   },
 
-  -- Navigate between panes
+  ----------------------------------------------------------------------
+  -- Pane navigation (same on every OS)
+  ----------------------------------------------------------------------
   {
     key = "h",
     mods = "ALT",
     action = wezterm.action.ActivatePaneDirection("Left"),
   },
+
   {
     key = "j",
     mods = "ALT",
     action = wezterm.action.ActivatePaneDirection("Down"),
   },
+
   {
     key = "k",
     mods = "ALT",
     action = wezterm.action.ActivatePaneDirection("Up"),
   },
+
   {
     key = "l",
     mods = "ALT",
