1562301e6a1720f0bd0a8843e32ee3a352fb5d81
Thoufiq Yahya <xpheno.yahya@mercedes-benz.com>
2026-07-31 10:50:06 +0530

    update font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,7 +13,7 @@ config.initial_cols = 120
 config.initial_rows = 28
 
 config.font = wezterm.font("FiraCode Nerd Font Mono")
-config.font_size = 15
+config.font_size = 14
 
 -- Available color schemes:
 -- https://wezfurlong.org/wezterm/colorschemes/index.html
