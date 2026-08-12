8a6eb262cab579168faee657b6504585dd8ecf78
Thawfeek Yahya <thawfeek_yahya@yahoo.co.in>
2026-08-12 22:35:59 +0530

    Add Tab naming feature

--- nvim_lazy/lua/plugins/lualine.lua
+++ nvim_lazy/lua/plugins/lualine.lua
@@ -1,17 +1,86 @@
 return {
   -- Statusline
   "nvim-lualine/lualine.nvim",
+
   config = function()
-  require('lualine').setup({
-    options = {
-      theme = 'auto', -- Change theme
-      section_separators = { left = '', right = '' },  -- Arrow effect
-      component_separators = { left = ' ', right = ' ' }, -- Small arrows
-    },
-    sections = {
-      lualine_c = { 'filename' },
-      lualine_x = { 'encoding', 'fileformat', 'filetype' }
-    }
-  })
-  end
+    local lualine = require("lualine")
+
+    -- Name the current tab
+    vim.api.nvim_create_user_command("TabName", function(opts)
+      vim.t[vim.fn.tabpagenr()].name = opts.args
+      lualine.refresh()
+    end, {
+      nargs = 1,
+    })
+
+    -- Generate tabline showing all tabs
+    local function tabline()
+      local tabs = {}
+      local current = vim.fn.tabpagenr()
+      local total = vim.fn.tabpagenr("$")
+
+      for i = 1, total do
+        local name = vim.t[i].name
+
+        -- If tab has no custom name, use the current buffer name
+        if not name or name == "" then
+          local buflist = vim.fn.tabpagebuflist(i)
+          local bufnr = buflist[1]
+          name = vim.fn.bufname(bufnr)
+
+          if name == "" then
+            name = "[No Name]"
+          else
+            name = vim.fn.fnamemodify(name, ":t")
+          end
+        end
+
+        -- Highlight the current tab
+        if i == current then
+          table.insert(
+            tabs,
+            "%#TabLineSel# " .. name .. " "
+          )
+        else
+          table.insert(
+            tabs,
+            "%#TabLine# " .. name .. " "
+          )
+        end
+      end
+
+      return table.concat(tabs, "%#TabLineFill#│")
+    end
+
+    lualine.setup({
+      options = {
+        theme = "auto",
+        section_separators = {
+          left = "",
+          right = "",
+        },
+        component_separators = {
+          left = " ",
+          right = " ",
+        },
+      },
+
+      -- Your existing statusline
+      sections = {
+        lualine_c = { "filename" },
+        lualine_x = {
+          "encoding",
+          "fileformat",
+          "filetype",
+        },
+      },
+
+      -- Tabline
+      tabline = {
+        lualine_a = {
+          tabline,
+        },
+      },
+    })
+  end,
 }
