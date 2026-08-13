8d02bb7de197e0d0876f75f3b312fa03085bc1f8
Thawfeek Yahya <thawfeek_yahya@yahoo.co.in>
2026-08-13 17:17:46 +0530

    Update vim maximzer plugin

--- nvim_lazy/lua/plugins/vim-maximizer.lua
+++ nvim_lazy/lua/plugins/vim-maximizer.lua
@@ -2,6 +2,6 @@
   "declancm/maximize.nvim",
   config = true,
   keys = {
-    { "<leader>m", "<cmd>lua require('maximize').toggle()<CR>", desc = "Toggle Maximize Split" }
+    { "<leader>M", "<cmd>lua require('maximize').toggle()<CR>", desc = "Toggle Maximize Split" }
   }
 }
